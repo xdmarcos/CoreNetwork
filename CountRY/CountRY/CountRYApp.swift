8b6815a99e589150bf6cab28cad3c1ec2527859a
Marcos González <xdmgzdev@gmail.com>
2024-01-08 09:43:50 +0100

    Add Packages

---
+++ CountRY/CountRY/CountRYApp.swift
@@ -0,0 +1,32 @@
+//
+//  CountRYApp.swift
+//  CountRY
+//
+//  Created by Marcos A. González Piñeiro on 04/01/2024.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct CountRYApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
