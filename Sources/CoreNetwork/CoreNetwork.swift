8125a0679022e4f2243efe7efe91480ea557b3ec
Marcos Gonzalez <xdmgzdev@gmail.com>
2025-06-17 11:53:10 -0500

    CN: Add package logic

--- Sources/CoreNetwork/CoreNetwork.swift
+++ Sources/CoreNetwork/CoreNetwork.swift
@@ -1,2 +1,16 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  CoreNetwork.swift
+//
+//
+//  Created by Marcos A. González Piñeiro on 19/12/2023.
+//
+
+
+// Export types
+
+public typealias CoreHTTPAuthorizationMethod = CoreHTTP.AuthorizationMethod
+public typealias CoreHTTPHeaderKey = CoreHTTP.HeaderKey
+public typealias CoreHTTPMethod = CoreHTTP.Method
+public typealias CoreHTTPMimeType = CoreHTTP.MimeType
+public typealias CoreHTTPScheme = CoreHTTP.Scheme
+public typealias CoreHTTPStatusCode = CoreHTTP.StatusCode
